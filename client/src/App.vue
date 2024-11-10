22e46a71b5d4093126598bad4c236c6d4d83c344
Tim Klingelhoefer <tim.klingelhoefer@student.hs-rm.de>
2024-11-10 03:01:53 +0100

    Removed Express Backend, Added SpringBoot Backend, Created Player & Lobby (Model,Service,Controller), Created Login View, StartView

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import Game from './components/Game.vue';
 import JumpAndRunGame from './components/JumpAndRunGame.vue';
+import CreatePlayer from './components/lobby/CreatePlayer.vue';
+import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <JumpAndRunGame></JumpAndRunGame>
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
+
 
 
 </style>
