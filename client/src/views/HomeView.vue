c421b4e872a247c3f9e93216bb1a4be51d0a3e46
Tim Klingelhoefer <tim.klingelhoefer@student.hs-rm.de>
2024-11-20 21:59:11 +0100

    Added LobbyCode Input Field

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,16 +2,21 @@
     <div class="background">
         <div class="button-form">
             <button class="lobby-button" @click="createLobby()">Create Lobby</button><br>
-            <button class="lobby-button">Join Lobby</button>
+            <div class="flex-child">
+                <input type="text" class="lobbycode-input" v-model="lobbyId" placeholder="Enter Lobby-Code">
+                <button class="join-lobby-button" @click="joinLobby()">Join Lobby</button>
+            </div>
         </div>
     </div>
 </template>
 <script setup>
 import axios from 'axios';
+import { ref } from 'vue'
 import { useLobbyStore } from '../stores/lobbyStore';
 import router from '../router/router';
 
 const lobbyStore = useLobbyStore();
+const lobbyId = ref("");
 
 async function createLobby(){
     try{
@@ -31,6 +36,7 @@ async function createLobby(){
 }
 async function joinLobby(){
     try{
+        router.push(`/lobby/${lobbyId.value}`);
         //open modal, enter lobby-id, then send api request to join
     }catch(err){
         console.log(err)
@@ -42,27 +48,62 @@ async function joinLobby(){
     .button-form{
         position: absolute; /* Positioniere relativ zur .background */
         top: 43%; /* Passe diesen Wert an, damit die Form genau unter dem Titel steht */
-        left: 50%; /* Zentriere die Form horizontal */
+        left: 53%; /* Zentriere die Form horizontal */
         transform: translate(-50%, -50%); /* Perfekte Zentrierung */
         width: 30%;
         border-radius: 10px;
         padding: 20px;
         text-align: center;
     }
-
+    .flex-child{
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+    }
     .lobby-button{
-        margin: 10px;
+        padding:10px;
+        margin-bottom: 10px;
+        font-weight: 500;
+        color:#262626;
+        background-color: rgba(217, 151, 72, 1);
+        border-radius: 5px;
+        border: solid 0px;
+        width: 100%;
+        box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.4);
+    }
+    .lobby-button:hover{
+        background-color: rgb(255, 170, 65);
+        box-shadow: 5px 3px 3px rgba(0, 0, 0, 0.4);
+    }
+
+    .join-lobby-button{
         padding:10px;
         font-weight: 500;
         color:#262626;
         background-color: rgba(217, 151, 72, 1);
-        border-radius: 10px;
+        border-radius: 5px;
         border: solid 0px;
         width: 50%;
         box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.4);
     }
-    .lobby-button:hover{
-    background-color: rgb(255, 170, 65);
-    box-shadow: 5px 3px 3px rgba(0, 0, 0, 0.4);
-}
+    .join-lobby-button:hover{
+        background-color: rgb(255, 170, 65);
+        box-shadow: 5px 3px 3px rgba(0, 0, 0, 0.4);
+    }
+
+
+
+    .lobbycode-input{
+        font-size:medium;
+        font-weight: 550;
+        padding:10px;
+        border-radius: 5px;
+        border: solid 0;
+        box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.4);
+        width: 100%;
+    }
+
+    .lobbycode-input:focus{
+        outline: none;
+    }
 </style>
